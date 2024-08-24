fec1d9deefb4694114d255cbe314df5a93c4be3d
zohaib <zohaibshafiq2005@gmail.com>
2024-08-24 23:49:41 +0500

    cache functions

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,9 +1,21 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { unstable_noStore } from "next/cache";
+
+// it will refetch data after every 5 seconds;
+export const revalidate = 5;
+
+// revalidate and dynamic are file wide cache management tools
+// it will always refresh every fetch request , no data is cached
+// export const dynamic = "force-dynamic";
+// is similar to { cache: 'no-store' }
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
+  // its similar to dymanic which disable cache but its function wide
+  unstable_noStore();
+  const response = await fetch("http://localhost:8080/messages", {
+    // next: { revalidate: 5},
     headers: {
-      'X-ID': 'page',
+      "X-ID": "page",
     },
   });
   const messages = await response.json();
